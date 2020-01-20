2b0a8503cfca374ecbebada8daa246b78f9f0058
Igor Martyushenko <ig0r.matyushenk0@mail.ru>
2020-01-20 16:34:44 +0300

    add mustache

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -5,13 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller //контроллер - моуль программы, который слушает запросы и возвращает какие-то данные
 public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+            @RequestParam(name="name", required=false, defaultValue="World") String name, Map<String,Object> model) {
+        model.put("name", name);
         return "greeting";
     }
 }
